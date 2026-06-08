67330fcb4e4a82e641f24b405cea6cdd77376483
Brother500 <luke.grundy@magd.ox.ac.uk>
2026-06-08 16:33:34 +0100

    Assignment 2 first update

--- scripts/plot.py
+++ scripts/plot.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 
 
 # parameters to modify 
-filename="my_input.txt"
-label='label'
-xlabel = 'xlabel'
-ylabel = 'ylabel'
-title='Simple plot'
-fig_name='my_graph.png'
-bins=100 #adjust the number of bins to your plot
+filename="../assignment2/millionreadshighp.txt"
+label='python'
+xlabel = 'time'
+ylabel = 'read'
+title='CDF of Million reads high performance mode'
+fig_name='CDFmillionreadshighp.png'
+bins=100000 #adjust the number of bins to your plot
 
 ## load data from input file
 t = np.loadtxt(filename, delimiter=" ", dtype="float")
@@ -19,16 +19,16 @@ t = np.loadtxt(filename, delimiter=" ", dtype="float")
 #plt.plot(t[:,0], t[:,1], label=label)  # Plot some data on the (implicit) axes.
 
 ## if your data is "X" (1 col), use the following line
-plt.plot(t, label=label)  # Plot some data on the (implicit) axes.
+#plt.plot(t, label=label)  # Plot some data on the (implicit) axes.
 
 ## comment the lines above and uncomment the line below to plot a simple CDF
 #plt.hist(t[:], bins, density=True, histtype='step', cumulative=True, label=label)
 
 ## comment the lines above and uncomment the 4 lines below for a nicer CDF
-#n = np.arange(1,len(t)+1) / float(len(t))
-#ts = np.sort(t)
-#fig, ax = plt.subplots()
-#ax.step(ts,n)
+n = np.arange(1,len(t)+1) / float(len(t))
+ts = np.sort(t)
+fig, ax = plt.subplots()
+ax.step(ts,n)
 
 plt.xlabel(xlabel)
 plt.ylabel(ylabel)
