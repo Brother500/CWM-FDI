970f67f0875e4f9f2510484ba8c0fbd095f588f4
Brother500 <luke.grundy@magd.ox.ac.uk>
2026-06-12 12:07:35 +0100

    Assignment 6 performance tester finished

--- assignment6/perf_tester.py
+++ assignment6/perf_tester.py
@@ -8,66 +8,75 @@ import numpy as np
 
 #### Variables  ####
 
-n = 5               # n = number of seconds to average over
+n = 10              # Number of samples to average over
+i = .01             # Time between samples in seconds
 
 #### Functions  ####
 
-## Find CPU usage at one moment
-# Source - https://forums.raspberrypi.com/viewtopic.php?t=22180
+## Get CPU usage at one moment
+# Source - https://stackoverflow.com/a/59214241
+# Posted by Giammo
+# Retrieved 2026-06-12, License - CC BY-SA 4.0
 def get_CPU_use():
-    return(str(os.popen("top -n1 | awk '/Cpu[(]s[)]:/ {print $2}'").readline().strip()))
+    return(str(os.popen("top -b -n1 | grep 'Cpu(s)' | awk '{print $2 + $4}'").readline().strip())) 
 
-## Find Memory usage at one moment
-# Source - https://stackoverflow.com/a/42275253 Posted by Hrabal, modified by community. See post 'Timeline' for change history, Retrieved 2026-06-11, License - CC BY-SA 4.0
+
+## Get Memory usage at one moment as a percentage
+# Source - https://stackoverflow.com/a/42275253
+# Posted by Hrabal, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-06-11, License - CC BY-SA 4.0
 def get_Memory_use():
-    tot_m, used_m, free_m = map(int, os.popen('free -t -m').readlines()[-1].split()[1:])
-    mem_pc = round(used_m/tot_m,2)
+    tot_m, used_m, free_m = map(int, os.popen('free -t -m').readlines()[-1].split()[1:])    # Find Memory capacity, Memory in use, & Memory free
+    mem_pc = 100*round(used_m/tot_m,2)                                                      # Find percentage of Memory capacity that is in use
     return(mem_pc)
 
-## Record CPU usage for n seconds
-def record_CPU_usage(CPU_array,n):
-    for i in range (0,n):           # Loop n times
+## Record CPU usage n times, i seconds apart, as float values in CPU_array
+def record_CPU_usage(CPU_array,n,i):
+    for j in range (0,n):               # Loop n times
         CPU_use_str = get_CPU_use()     # Get CPU usage
-        time.sleep(1)               # Wait 1 second
-        CPU_use = float(CPU_use_str)
-        CPU_array.append(CPU_use)   # Add CPU usage to array
-    return(CPU_array)# Return array of CPU usage
+        time.sleep(i)                   # Wait i (interval) seconds
+        CPU_use = float(CPU_use_str)    # Convert to float for use in calculations
+        CPU_array.append(CPU_use)       # Add CPU usage to array
+    return(CPU_array)                   # Return array of CPU usage
 
-## Record Memory usage for n seconds
-def record_Memory_usage(Memory_array,n):
-    for i in range(0,n):                # Loop n times
+## Record Memory usage n times, i seconds apart, in Memory_array
+def record_Memory_usage(Memory_array,n,i):
+    for j in range(0,n):                # Loop n times
         Memory_use = get_Memory_use()   # Get Memory usage
-        time.sleep(1)                   # Wait 1 second
+        time.sleep(i)                   # Wait i (interval) seconds
         Memory_array.append(Memory_use) # Add Memory usage to array
     return(Memory_array)                # Return array of Memory usage
 
-## Find avg of array
+## Find average value of array, and return this value
 def find_array_avg(array):
     array_length = len(array)               # Find length of array
-    array_sum = np.sum(array)                  # Find sum of array
-    array_avg = array_sum / array_length    # Find avg of array
+    array_sum = np.sum(array)               # Find sum of array
+    array_avg = array_sum / array_length    # Find average of array
     return(array_avg)
 
 
 #### Main code  ####
 
-CPU_bottleneck = False                                  # It is assumed that CPU and Memory are not bottlenecks until proven otherwise
+CPU_bottleneck = False      # It is assumed that CPU and Memory are not bottlenecks until proven otherwise
 Memory_bottleneck = False
-CPU_usage = []
+CPU_usage = []              # Initialise empty arrays for CPU & Memory usage
 Memory_usage = []
 
-CPU_usage = record_CPU_usage(CPU_usage,n)                         # Record CPU usage for n seconds
-Memory_usage = record_Memory_usage(Memory_usage,n)                   # Record Memory usage for n seconds
-CPU_avg = find_array_avg(CPU_usage)                     # Find avg CPU usage
-Memory_avg = find_array_avg(Memory_usage)               # Find avg Memory usage
-if CPU_avg >= 95:                                     # Bottleneck if avg CPU usage > 95%
+CPU_usage = record_CPU_usage(CPU_usage,n,i)                 # Record CPU usage n times, i seconds apart
+Memory_usage = record_Memory_usage(Memory_usage,n,i)        # Record Memory usage n times, i seconds apart
+CPU_avg = find_array_avg(CPU_usage)                         # Find average CPU usage
+Memory_avg = find_array_avg(Memory_usage)                   # Find average Memory usage
+print("CPU average % is:", CPU_avg, "Memory average % is", Memory_avg)
+if CPU_avg >= 95:                                           # Bottleneck if average CPU usage > 95%
     CPU_bottleneck = True
-if Memory_avg >= 0.80:                                  # Bottleneck if avg Memory usage > 80%
-    Memory_bottleneck = False
-if CPU_bottleneck == True or Memory_bottleneck == True: # Report any bottlenecks
-    if CPU_bottleneck == True:                          # Propose solutions to bottlenecks
-        print("CPU usage averages over 95%! This is a bottleneck. Consider upgrading to a more powerful CPU.")
-    if Memory_bottleneck == True:
-        print("Memory usage averages over 80! This is a bottleneck. Consider upgrading to a larger amount of RAM.")
+if Memory_avg >= 80:                                        # Bottleneck if average Memory usage > 80%
+    Memory_bottleneck = True
+if CPU_bottleneck == True and Memory_bottleneck == True:    # If both are high, neither is a bottleneck
+    print("Both CPU and Memory are utilised well, neither is a bottleneck for this program.")
 else:
-    print("No bottlenecks! Try a more strenuous program.")
+    if CPU_bottleneck == True:                              # Report & propose solutions to bottlenecks
+        print("CPU usage averages over 95%! This is a bottleneck. Consider upgrading to a more powerful CPU.")
+    elif Memory_bottleneck == True:
+        print("Memory usage averages over 80%! This is a bottleneck. Consider closing tabs and programs that are not in use or upgrading to a larger amount of RAM.")
+    else:                                                   # Neither is high so cannot tell if there is a bottleneck
+        print("Not enough load to tell if there is a bottleneck. Try a more strenuous program.")
